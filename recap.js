09bfd5100edc77f58420bf7b917cd815ee630dd0
sabbir1257 <masabbir1257@gmail.com>
2024-10-25 22:47:15 +0600

    Introduction and Basic ES6 Recap

---
+++ recap.js
@@ -0,0 +1,11 @@
+/**
+ * 1. var let const 
+ * 2. default parameter
+ * 3. template string 
+ * 4. arrow function 
+ * 5. destructuring and spread operator
+ * 6. Object.keys, Object.valuse, Object.entries
+ * 7. for of used in array and string 
+ * 8. for in loop used in object 
+ * 
+ * */ 
